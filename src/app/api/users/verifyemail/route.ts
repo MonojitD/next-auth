6babc2b3ad76fe328178ddb8d817c00de7c20b71
MonojitD <monojitdeb66@gmail.com>
2024-08-03 21:32:44 +0530

    Bug fix 1

--- src/app/api/users/verifyemail/route.ts
+++ src/app/api/users/verifyemail/route.ts
@@ -14,7 +14,7 @@ export async function POST(request: NextRequest) {
 
         if (!user) {
             return NextResponse.json(
-                { message: "User not found" },
+                { message: "Invalid token" },
                 { status: 400 }
             )
         }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,6 @@ export default function Home() {
           <Link
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
             href="/profile"
-            target="_blank"
             rel="noopener noreferrer"
           >
             By{" "}
